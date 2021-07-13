2b49f8cb355d8d281e32698c3f20b24cc4418bbf
jad dawani <dawanijad@gmail.com>
2021-07-13 20:47:42 +0300

    react assignments

---
+++ MERN/React/my-first-app/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Hello Jad
+      </header>
+      <div> <h1> Hello Dojo</h1>
+      <h2>Things i need to do</h2>
+      <u1>
+        <li>learnreact</li>
+         <li>Climb</li>
+        <li>Run</li>
+        <li>Feed</li>
+      </u1>
+      </div> 
+
+    </div>
+  );
+}
+
+export default App;
